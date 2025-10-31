100c095f6afffbf76eb1981e5702ece51aafe4f2
taishin <ok220259@oks.iput.ac.jp>
2025-10-31 18:08:14 +0900

    main.py

--- main.py
+++ main.py
@@ -32,9 +32,13 @@ from omni.timeline import get_timeline_interface
 from omni.isaac.dynamic_control import _dynamic_control as dc
 from omni.isaac.core.prims import RigidPrim
 
+cmd_velocity = 0.0
+work_coordinate = []
 def on_message(client, userdata, msg):
-    data = msg.payload
-    data = data.split(",")
+    if msg.topic == "/feedback/velocity":
+        cmd_velocity = msg.payload
+    elif msg.topic == "/vision/work/coordinate":
+        work_coordinate = msg.payload.split(',')
 
 sub = MQTT_SUB(ip_addr='192.168.11.20', port=1883, keep_alive=60, topic='real_feedback_data',
                on_connect=None,on_disconnect=None, on_message=on_message)
